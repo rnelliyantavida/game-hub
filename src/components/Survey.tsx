d9a382e578d0fd3b93b8edf6aa57b600b35b5115
rnelliyantavida <rnelliyantavida@miraclesoft.com>
2025-03-16 14:43:33 -0400

    added survey in dialogue

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -1,36 +1,57 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Button,
-    useDisclosure,
-  } from "@chakra-ui/react";
-  
-  function Survey() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-  
-    return (
-      <>
-        <Button onClick={onOpen}>Open Dialog</Button>
-  
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>My Custom Dialog</ModalHeader>
-            <ModalBody>Here’s your dialog content.</ModalBody>
-            <ModalFooter>
-              <Button colorScheme="blue" onClick={onClose}>
-                Close
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  }
-  
-  export default Survey;
-  
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+function Survey() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <button
+        onClick={onOpen}
+        style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          padding: "15px",
+          fontSize: "16px",
+          backgroundColor: "#4CAF50",
+          color: "#fff",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Give Feedback
+      </button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          {/* <ModalHeader>My Custom Dialog</ModalHeader> */}
+          <ModalBody>
+            <iframe
+              src="https://qualtricsxmrcdv9zxbz.qualtrics.com/jfe/form/SV_5jOy4YtMuTA8cjc?FirstName=Ruksana"
+              width="100%"
+              height="400px"
+            ></iframe>
+          </ModalBody>
+          {/* <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter> */}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default Survey;
